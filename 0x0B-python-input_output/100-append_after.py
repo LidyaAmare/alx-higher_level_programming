312d8e778b52dabdc0dc8f305145d0ced86934ee
LidyaAmare <lidyaamare76@gmail.com>
2022-10-25 23:05:50 -0700

    done

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" function that append a new line
-"""
+"""Defines a text file insertion function."""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """ function append_after(filename="", search_string="", new_string=""):
+    """Insert text after each line containing a given string in a file.
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
     """
-    _line = []
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            _line += [line]
-            if line.find(search_string) != -1:
-                _line += [new_string]
-
-    with open(filename, 'w', encoding="utf-8") as f:
-        f.write("".json(_line))
+    text = ""
+    with open(filename) as r:
+        for line in r:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as w:
+        w.write(text)
