7350f6262558da7340431c780d9eddfe83ab8f7e
LidyaAmare <lidyaamare76@gmali.com>
2022-11-30 11:06:56 +0300

    done

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,6 @@
-#!/usr/bin/env bash
-#DESCRIPTION OF FUNCTIONALITY
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
